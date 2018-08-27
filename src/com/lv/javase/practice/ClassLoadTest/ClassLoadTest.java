69aaf6e7b566a59a32b9c064380e4725ecc590af
angie1115 <522516400@qq.com>
2018-08-27 16:35:27 +0800

    类加载

--- src/com/lv/javase/practice/ClassLoadTest/ClassLoadTest.java
+++ src/com/lv/javase/practice/ClassLoadTest/ClassLoadTest.java
@@ -1,6 +1,8 @@
 package com.lv.javase.practice.ClassLoadTest;
 
+import com.lv.javase.practice.ClassLoadTest.myClassLoad.Parent2;
 import org.junit.jupiter.api.Test;
+import sun.misc.ClassLoaderUtil;
 
 import java.util.Arrays;
 
@@ -21,5 +23,29 @@ public class ClassLoadTest {
         System.out.println("*****java.class.path******");
         String[] paths = System.getProperty("java.class.path").split(";");
         Arrays.stream(paths).forEach(p-> System.out.println(p));
+
+        //********ClassLoad
+        System.out.println("this.getClass().getClassLoader():"+this.getClass().getClassLoader());
+        System.out.println("Thread.currentThread().getContextClassLoader():"+Thread.currentThread().getContextClassLoader());
+        System.out.println("ClassLoadTest.class.getClassLoader():"+ClassLoadTest.class.getClassLoader());
+        System.out.println("Parent2.class.getClassLoader():"+Parent2.class.getClassLoader());
+        System.out.println(Parent2.class);
+        try {
+            //执行类的静态块 类的静态快只加载一次
+            Class<?> parent2 = Class.forName("com.lv.javase.practice.ClassLoadTest.myClassLoad.Parent2");
+            System.out.println("Parent2.class==parent2:"+(Parent2.class==parent2));
+            System.out.println("class getClassLoader:"+parent2.getClassLoader());
+            System.out.println(Class.forName("com.lv.javase.practice.ClassLoadTest.myClassLoad.Parent2"));
+            Parent2 p = (Parent2) parent2.newInstance();
+            //不执行类的静态块
+            ClassLoader.getSystemClassLoader().loadClass("com.lv.javase.practice.ClassLoadTest.myClassLoad.Parent2").newInstance();
+            new Parent2();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
     }
 }
